d21e153eb6f9a1eb77433ed73304406274858b5c
Bradley Gohman <bradley.s.gohman@gmail.com>
2015-04-19 08:42:25 -0500

    associations-assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   has_many :questions
   belongs_to :user
 
-default_scope { order('created_at DESC') }
+# default_scope { order('created_at DESC') }
   scope :ordered_by_title, -> { order('title ASC') }
   scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
 end
