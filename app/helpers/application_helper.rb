92e5acbf53db76116877903d5fcd19bba6b1dca7
Bradley Gohman <bradley.s.gohman@gmail.com>
2015-04-26 21:37:22 -0500

    validating posts checkpoint

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def my_name
     "Bradley Gohman"
   end
+
+  def form_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form-group'
+    end
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,12 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
 
-# default_scope { order('created_at DESC') }
+  default_scope { order('created_at DESC') }
   scope :ordered_by_title, -> { order('title ASC') }
   scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 end
