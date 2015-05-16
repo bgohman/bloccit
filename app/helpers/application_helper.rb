7d20d4656d6f76fdecfb140dd99c9d3a8e18a6b0
Bradley Gohman <bradley.s.gohman@gmail.com>
2015-05-16 09:44:59 -0500

    another-interlude-assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,12 @@ module ApplicationHelper
     (redcarpet.render markdown).html_safe
   end
 
+  def up_vote_link_classes(post)
+    "glyphicon glyphicon-chevron-up #{(current_user.voted(post) && current_user.voted(post).up_vote?) ? 'voted' : '' }"
+  end
+
+  def down_vote_link_classes(post)
+    "glyphicon glyphicon-chevron-down #{(current_user.voted(post) && current_user.voted(post).down_vote?) ? 'voted' : '' }"
+  end
+
 end
